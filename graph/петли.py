3622ce11a69804f327e0fe024a54a3dba67b31b8
Катя <user3737373737@github.ru>
2024-02-26 14:24:21 +0300

    Add README.md

--- graph/петли.py
+++
@@ -1,7 +0,0 @@
-lst = [list(map(int, input().split())) for i in range(int(input()))]
-flag = 'NO'
-for i in range(len(lst)):
-    for j in range(len(lst)):
-        if lst[i][i] == 1:
-            flag = 'YES'
-print(flag)
--- graph/проверка на неориентированность.py
+++
@@ -1,7 +0,0 @@
-lst = [list(map(int, input().split())) for i in range(int(input()))]
-flag = 'NO'
-for i in range(1, len(lst)):
-    for j in range(i):
-        if lst[i][i] == 1 and lst[i][j] == lst[j][i]:
-            flag = 'YES'
-print(flag)

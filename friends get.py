cb60e604087b8e978578bdad6baa81315779bc49
Катя <user3737373737@github.ru>
2024-03-07 00:57:18 +0300

    preview of graph

--- friends get.py
+++ friends get.py
@@ -1,27 +1,50 @@
+from pyvis.network import Network
 import vk_api
+import networkx as nx
+
+g = nx.Graph()
 
 
-token = ''
+new_token = 'vk1.a.cahpljxwFA-ZxoYFpedjUhpDYY6rFtx7yHJZwQP0dqgUKwGG_TmFdf73hrhO7_uQ3HlIzUc-BCOlNJ0Q2gtBDzZXxT02j8oZ1_xYHceR6QvJsgxYOIq73Yo7w2IpqGYO8uxewxvRWy2pVL9BzV9ZpJJxAugBcDxsZkW6ToGum9luD42ANLE7IU-Du1uU2M2ljIz47KmFl9WtgonvZCAEcQ'
 
-session = vk_api.VkApi(token=token)
+session = vk_api.VkApi(token=new_token)
 vk = session.get_api()
 
+d = {}
 
-def get_friends(user_id):
-    friends = session.method("friends.get", {"user_id": user_id})
-    for friend in friends["items"]:
-        user = session.method("users.get", {"user_ids": friend,
-                                            "fields": "city, sex"})
-        name = user[0]["last_name"]
-        surname = user[0]["first_name"]
-        if user[0]["sex"] == 1:
-            sex = 'жен'
+
+def get_friends(user_id, d):
+    friends1 = session.method("friends.get", {"user_id": user_id,
+                                              "fields": "bdate, city, sex"})
+    for i in friends1["items"]:
+        d[i["id"]] = []
+        if i["is_closed"]:
+            d[i["id"]] = 'private account'
+        elif 'deactivated' in i:
+            d[i["id"]] = 'banned account'
         else:
-            sex = 'муж'
-        # birth = user[0]["bdate"]
-        # city = user[0]["city"]
-        # photo = user[0]["photo_id"]
-        print(name, surname, sex)
+            d[i["id"]] = session.method("friends.get", {"user_id": i["id"],
+                                                        "fields": "bdate, city, sex"})
+    return d
 
 
-get_friends(1)
+def draw_friends(user_id, dict):
+    for i in dict:
+        g.add_node(i, label=i, color='blue')
+        g.add_edge(user_id, i)
+
+
+def draw(d):
+    for t in d:
+        if d[t] != 'private account' and d[t] != 'banned account':
+            for q in d[t]["items"]:
+                g.add_node(q["id"], label=q["first_name"], color='yellow')
+                g.add_edge(t, q["id"])
+
+
+get_friends(218756086, d)
+draw_friends(218756086, d)
+draw(d)
+nt = Network(notebook=True, cdn_resources='remote')
+nt.from_nx(g)
+nt.show('graph.html')
